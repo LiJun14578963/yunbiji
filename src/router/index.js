b75540f768821f09f51d208921712803f7911f1b
lijun <1094453726@qq.com>
2022-09-18 17:46:43 +0800

    懒加载后代码提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../components/Login'
-import Notebooks from '../components/NotebookList.vue'
-import NoteDetail from '../components/NoteDetail.vue'
-import TrashDetail from '../components/TrashDetail.vue'
+
+// import Notebooks from '../components/NotebookList.vue'
+// import NoteDetail from '../components/NoteDetail.vue'
+// import TrashDetail from '../components/TrashDetail.vue'
+// import Login from '../components/Login'
 
 Vue.use(Router)
 
@@ -12,19 +13,19 @@ export default new Router({
     {
       path: '/',
       alias: '/notebooks',
-      component: Notebooks
+      component: () => import('../components/NotebookList.vue')
     },
     {
       path: '/login',
-      component: Login
+      component: () => import('../components/Login')
     },
     {
       path: '/note',
-      component: NoteDetail
+      component: () => import('../components/NoteDetail.vue')
     },
     {
       path: '/trash',
-      component: TrashDetail
+      component: () => import('../components/TrashDetail.vue')
     }
   ]
 })
